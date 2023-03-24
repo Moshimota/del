7d77cb83cfd3fb701a4106dbdaa86aa631f152d5
Moshimota <dasercasseigzez@gmail.com>
2023-03-25 01:07:42 +0500

    siuuuuuuuuuuu

--- main.js
+++ main.js
@@ -1,34 +1,16 @@
-const getData = async () => {
-    const url = await fetch("http://localhost:3000/data")
-    const json = await url.json()
-    console.log(json);
-
-    
-    const list = document.querySelector(".list-group")
-    json?.map((res) => {
-        list.innerHTML +=`
-        <li class="list-group-item"><div><b>Name : </b>${res?.name}</div> <button class="btn2">delete</button></li>
-        `
-        const btn2 = document.querySelector(".btn2")
-        btn2.onclick = () => {
-            list.style.display = "none"
-        }
-    })
-}
-getData()
-const btn1 = document.querySelector(".postData")
-btn1.onclick = (e) => {
-    e.preventDefault()
-    postData()
-}
-const postData = () => {
-    const inp = document.querySelector(".form-control")
-    const val = inp.value
-    fetch("http://localhost:3000/data", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({name: val})
-    }).then((msg) => alert(msg.statusText))
-}
+document.addEventListener("keydown", e => {
+    if (e.key == "Delete") {
+        const hed = document.querySelector(".kak")
+        const inp = document.querySelector(".form-control")
+        hed.innerHTML = ""
+        inp.value = ""
+    }else if (e.key == "Enter"){
+        const hed = document.querySelector(".kak")
+        const inp = document.querySelector(".form-control")
+        hed.innerHTML =inp.value
+        inp.value = ""
+    }
+})
+const hed = document.querySelector(".kak")
+        const inp = document.querySelector(".form-control")
+        hed.innerHTML = ""
